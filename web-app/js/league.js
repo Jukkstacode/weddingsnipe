bf17996bd43846221d8f8569a44d2048fc6c3c56
Chris Bimm <chris.bimm@gmail.com>
2026-09-13 22:12:20 -0700

    Allow 4 years

--- web-app/js/league.js
+++ web-app/js/league.js
@@ -1,2 +1,2 @@
 export const GMS = ['Adam', 'Andy', 'Bimm', 'Charlie', 'Colin', 'Dan', 'Dave', 'Hordo', 'Jordan', 'Marinos', 'Mike', 'Ryan', 'Seedo', 'Trevor'];
-export const MAX_CONTRACT_YEARS = 3;
+export const MAX_CONTRACT_YEARS = 4;
